050ae366b8a31eee480fc4f3191111756dc7454c
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-12-04 18:15:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,51 @@
-
 import React from "react";
-import './../styles/App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { updateName, updateEmail } from "../redux/actions";
 
 const App = () => {
+  const { name, email } = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const handleNameChange = (e) => {
+    dispatch(updateName(e.target.value));
+  };
+
+  const handleEmailChange = (e) => {
+    dispatch(updateEmail(e.target.value));
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <h1>User Information</h1>
 
-export default App
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+          placeholder="Enter your name"
+        />
+      </label>
+
+      <br />
+
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          placeholder="Enter your email"
+        />
+      </label>
+
+      <div className="output">
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
